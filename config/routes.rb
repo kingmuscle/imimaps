bd4a5e8dd4e36b041a4ea0ce0a963886d210b19f
Daniil Stepanov <danistep@mail.ru>
2017-07-12 15:49:48 +0200

    commented out old routes

--- config/routes.rb
+++ config/routes.rb
@@ -68,9 +68,9 @@ ImiMaps::Application.routes.draw do
 
 		#root to: 'sessions#new'
 
-    get 'signup', to: 'users#new', as: 'signup'
-    get 'login', to: 'sessions#new', as: 'login'
-    get 'logout', to: 'sessions#destroy', as: 'logout'
+    # get 'signup', to: 'users#new', as: 'signup'
+    # get 'login', to: 'sessions#new', as: 'login'
+    # get 'logout', to: 'sessions#destroy', as: 'logout'
 
 end
 	end
